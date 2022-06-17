9d8c2f7169cf7a2bcb773e1cf854d95567425c79
KulubekEgemberdiev <askatovich61.@gmail.com>
2022-06-17 15:54:31 +0600

    Added views to all pages

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_view(request):
+    return render(request, "index.html")
+
+
+def first_view(request):
+    return render(request, "first_page.html")
+
+
+def second_view(request):
+    return render(request, "second_page.html")
+
+
+def third_view(request):
+    return render(request, "third_page.html")
